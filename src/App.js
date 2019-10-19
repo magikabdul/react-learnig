d9ad806ff7f0d9e6b4336e7b51955b5e6190f288
Krzysztof Cholewa (Home) <krzysztof.cholewa@gmail.com>
2019-10-19 12:40:47 +0200

    chore: referencing table as props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 
 class ToDoList extends Component {
    render() {
-      return <h1>{this.props.title}</h1>;
+      return (
+         <div>
+            <h1>{this.props.title}</h1>
+            {this.props.tasks.map(task => (
+               <div>{task}</div>
+            ))}
+         </div>
+      );
    }
 }
 
 class App extends Component {
+   myTasks = ['Record a ReactJS video', 'Go for a walk'];
+
    render() {
       return (
          <div>
-            <ToDoList title="My stuff" />
+            <ToDoList title="My stuff" tasks={this.myTasks} />
          </div>
       );
    }
