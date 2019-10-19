968386c5bd34590b790d37ee0d64335e425c6087
Krzysztof Cholewa (Home) <krzysztof.cholewa@gmail.com>
2019-10-19 19:12:08 +0200

    feat: communictaion with API

--- src/containers/ToDoList/ToDoList.js
+++ src/containers/ToDoList/ToDoList.js
@@ -2,26 +2,36 @@ import React, { Component } from 'react';
 import { ToDoItem } from '../../components/ToDoItem/ToDoItem';
 import { NewToDoForm } from '../../components/NewToDoForm/NewToDoForm';
 export class ToDoList extends Component {
-	state = {
-		tasks: this.props.tasks,
-		draft: ''
-	};
-	updateDraft = event => {
-		this.setState({ draft: event.target.value });
-	};
-	addToDo = () => {
-		const { tasks, draft } = this.state;
-		const list = tasks;
-		list.push({ text: draft, done: false });
-		this.setState({ tasks: list, draft: '' });
-	};
-	render() {
-		const { title } = this.props;
-		const { tasks, draft } = this.state;
-		return (<div className="container">
-			<h1>{title}</h1>
-			{tasks.map(task => (<ToDoItem text={task.text} done={task.done} />))}
-			<NewToDoForm onSubmit={this.addToDo} onChange={this.updateDraft} draft={draft} />
-		</div>);
-	}
+   componentDidMount() {
+      fetch('https://api.github.com/users/magikabdul/repos')
+         .then(response => response.json())
+         .then(json => this.setState({ tasks: json }));
+   }
+
+   state = {
+      tasks: this.props.tasks,
+      draft: ''
+   };
+   updateDraft = event => {
+      this.setState({ draft: event.target.value });
+   };
+   addToDo = () => {
+      const { tasks, draft } = this.state;
+      const list = tasks;
+      list.push({ text: draft, done: false });
+      this.setState({ tasks: list, draft: '' });
+   };
+   render() {
+      const { title } = this.props;
+      const { tasks, draft } = this.state;
+      return (
+         <div className="container">
+            <h1>{title}</h1>
+            {tasks.map(task => (
+               <ToDoItem text={task.name} done={task.done} />
+            ))}
+            <NewToDoForm onSubmit={this.addToDo} onChange={this.updateDraft} draft={draft} />
+         </div>
+      );
+   }
 }
