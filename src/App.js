0c306fc10609ec7d8976974bc2aaf107f1fd9bca
Krzysztof Cholewa (Home) <krzysztof.cholewa@gmail.com>
2019-10-19 13:21:27 +0200

    chore: state and props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
 class ToDoList extends Component {
+   state = {
+      tasks: this.props.tasks,
+      draft: ''
+   };
+
+   updateDraft = event => {
+      this.setState({ draft: event.target.value });
+   };
+
+   addToDo = () => {
+      //destruction
+      const { tasks, draft } = this.state;
+
+      //without destruction
+      const list = tasks;
+
+      list.push(draft);
+      this.setState({ tasks: list, draft: '' });
+   };
+
    render() {
+      const { title } = this.props;
+      const { tasks, draft } = this.state;
+
       return (
          <div>
-            <h1>{this.props.title}</h1>
-            {this.props.tasks.map(task => (
+            <h1>{title}</h1>
+            {tasks.map(task => (
                <div>{task}</div>
             ))}
+            <input type="text" onChange={this.updateDraft} value={draft} />
+            <button onClick={this.addToDo}>Add</button>
          </div>
       );
    }
